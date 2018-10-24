e3950bbe211dfba174a7555907f407b1009ce763
andradnl <andra.danalachi@gmail.com>
2018-10-24 15:38:21 +0300

    added shadow on navbar

--- WatchAppFinal/src/components/landing.components/NavBar.js
+++ WatchAppFinal/src/components/landing.components/NavBar.js
@@ -1,26 +1,26 @@
-import React from 'react'
-import { Text, View, TouchableOpacity } from 'react-native'
-import { withNavigation } from 'react-navigation'
-import styled from 'styled-components'
+import React from "react"
+import { Text, View, TouchableOpacity } from "react-native"
+import { withNavigation } from "react-navigation"
+import styled from "styled-components"
 
 class NavBar extends React.Component {
   render() {
     const { navigate } = this.props.navigation
     return (
       <Wrapper>
-        <ButtonWrapper onPress={() => navigate('Shop')}>
+        <ButtonWrapper onPress={() => navigate("Shop")}>
           <CurrentButtonText>Shop</CurrentButtonText>
         </ButtonWrapper>
 
-        <ButtonWrapper onPress={() => navigate('Discover')}>
+        <ButtonWrapper onPress={() => navigate("Discover")}>
           <ButtonText>Discover</ButtonText>
         </ButtonWrapper>
 
-        <ButtonWrapper onPress={() => navigate('Learn')}>
+        <ButtonWrapper onPress={() => navigate("Learn")}>
           <ButtonText>Learn</ButtonText>
         </ButtonWrapper>
 
-        <ButtonWrapper onPress={() => navigate('Profile')}>
+        <ButtonWrapper onPress={() => navigate("Profile")}>
           <ButtonText>Me</ButtonText>
         </ButtonWrapper>
       </Wrapper>
@@ -29,23 +29,24 @@ class NavBar extends React.Component {
 }
 
 const Wrapper = styled(View)`
-  border-color: #e3e3e3;
-  border-top-width: 5;
-  position: relative;
-  flex: 1;
+  margin-top: 20;
+  background-color: white;
   justify-content: space-around;
   flex-direction: row;
+  box-shadow: 0 -10px 5px lightgrey;
 `
 const ButtonWrapper = styled(TouchableOpacity)`
   padding-vertical: 20;
 `
 const ButtonText = styled(Text)`
+  color: #646464;
   font-size: 15;
   font-family: Graphik-Regular;
 `
 const CurrentButtonText = styled(Text)`
   font-size: 15;
   font-family: Graphik-Medium;
-  color: #7325FB;  
+  color: #7325fb;
 `
+
 export default withNavigation(NavBar)
