5cd14e5b7efbf1f2ea839ccc7e46f5e72215f0d2
andradnl <andra.danalachi@gmail.com>
2018-10-25 10:41:33 +0300

    modified info swiper path

--- WatchAppFinal/src/screens/DetailsScreen.js
+++ WatchAppFinal/src/screens/DetailsScreen.js
@@ -5,7 +5,7 @@ import { ScrollView } from 'react-native'
 import ImageSwiper from '../components/details.components/ImageSwiper'
 import NameInfo from '../components/details.components/NameInfo'
 import PriceInfo from '../components/details.components/PriceInfo'
-import InfoSwiper from '../components/details.components/infoswiper.components/InfoSwiper'
+import InfoSwiper from '../components/details.components/InfoSwiper/InfoSwiper'
 import SimilarWatches from '../components/details.components/SimilarWatches'
 import Guarantee from '../components/details.components/Guarantee'
 import BuyButton from '../components/details.components/BuyButton'
@@ -13,7 +13,6 @@ import BuyButton from '../components/details.components/BuyButton'
 class DetailsScreen extends React.Component {
   render() {
     const model = this.props.navigation.getParam('model', 'NO-DATA')
-    console.log('[DetailsScreenData]', model)
     return (
       <ScrollView>
         <ImageSwiper photos={model.Photos} />
